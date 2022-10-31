3c8d06a30e175dfc33c73a2f63e51d5a2e0b822c
ezra02 <ezrafikadu02@gmail.com>
2022-10-31 16:58:17 +0300

    home page fetch latest products and display

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_spinkit/flutter_spinkit.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
 void main() {
   runApp(const MyApp());
 }
@@ -31,6 +33,17 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   final searchController=TextEditingController();
+  List<dynamic> items=[];
+  Future<void> getItems() async{
+    var res=await http.get(Uri.parse('http://127.0.0.1:8001/videos'));
+    items=jsonDecode(res.body);
+  } 
+  
+  @override
+  void initState() {
+    getItems();
+    super.initState();
+  }
 
   @override
   void dispose() {
@@ -63,12 +76,39 @@ class _MyHomePageState extends State<MyHomePage> {
           ),
         ]
       ),
-      body: Center(
+      body: items.length==0 ?
+      const Center(
         child: SpinKitFadingCube(
           size:140,
           color: Color(0Xff43db80)
         )
-      ),
+      )
+      :ListView.builder(
+        itemCount: items.length,
+        itemBuilder: (context,index){
+          return Container(
+            child:Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              children: [
+                Image.network('https://cdn.pixabay.com/photo/2022/09/26/23/26/african-american-7481724_960_720.jpg'),
+                Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    Text('item title',style:TextStyle(fontSize:17,fontWeight: FontWeight.w600)),
+                    Text('item.description',style:TextStyle(fontSize:15,color: Colors.grey[600]))
+                  ]
+                )
+              ]
+            ),
+            padding: EdgeInsets.all(5),
+            margin: EdgeInsets.all(5),
+            decoration: BoxDecoration(
+              color:Colors.white,
+              borderRadius: BorderRadius.circular(5)
+            ), 
+          );
+        },
+      )
     );
   }
 }
